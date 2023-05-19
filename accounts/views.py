26d44a23cbb2aae82d54a3bd3c9c7d7c1e7fd61c
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-05-19 12:51:56 +0530

    rendered login and register pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_login(request):
+    return render(request, 'accounts/login.html')
+
+def user_register(request):
+    return render(request, 'accounts/register.html')
